980be037fcc4fbe46c84d1c3500d254d5c93740a
Louis <louis.potterie@efrei.net>
2019-02-18 03:04:23 -0500

    Classe fenetre

---
+++ src/com/company/Fenetre.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import javax.swing.*;
+
+    public class Fenetre extends JFrame {
+
+        public Fenetre() {
+
+            JFrame fenetre = new JFrame();
+            fenetre.setTitle("TP3 Java");
+            fenetre.setSize(400, 400);
+            fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            fenetre.setVisible(true);
+        }
+    }
+
